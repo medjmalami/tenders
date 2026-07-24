a7fe913c7ce98e6ad89c81d10aaf2b2eb6949ef1
amine <jmalmohamedamine1@gmail.com>
2026-07-24 21:51:46 +0100

    moved import at the top

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -6,7 +6,9 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from src.db.base import Base
 from src.db.session import DATABASE_URL
+from src.models import user
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,9 +22,6 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from src.db.base import Base
-from src.models import user
-
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
